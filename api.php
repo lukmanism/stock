8683fa2dc63b838b79807d22e4e56b947ace4e7e
Lukman Hussein <lukmanism@gmail.com>
2016-03-14 16:25:01 +0800

    fresh copies

---
+++ api.php
@@ -0,0 +1,10 @@
+<?php
+error_reporting(0);
+
+
+$filename = "ftp://ftp.nasdaqtrader.com/SymbolDirectory/nasdaqlisted.txt";
+$handle = fopen($filename, "r");
+$contents = fread($handle, filesize($filename));
+fclose($handle);
+
+echo $contents;
