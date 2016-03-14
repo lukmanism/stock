79f39317e454e033135555c883d04e25a8a02393
Lukman Hussein <lukmanism@gmail.com>
2016-03-14 17:58:48 +0800

    added webservice call

--- api.php
+++ api.php
@@ -1,7 +1,6 @@
 <?php
 error_reporting(0);
 
-
 $filename = "ftp://ftp.nasdaqtrader.com/SymbolDirectory/nasdaqlisted.txt";
 $handle = fopen($filename, "r");
 $contents = fread($handle, filesize($filename));
